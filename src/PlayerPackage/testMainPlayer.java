8cd476bcb3da120c7c6fb6f908c6df07465d8f66
Urfen <arvidbod@kth.se>
2015-10-08 22:04:43 +0200

    money added to player

--- src/PlayerPackage/testMainPlayer.java
+++ src/PlayerPackage/testMainPlayer.java
@@ -1,5 +1,6 @@
 package PlayerPackage;
 
+import CardPackage.*;
 /**
  * Created by arvidbodin on 08/10/15.
  *
@@ -9,6 +10,8 @@ public class testMainPlayer {
     public static void main(String[] args) {
 
         Player p1,p2, computer;
+        Deck theDeck = new Deck();
+        theDeck.shuffleCards();
 
         p1 = new Player("Arvid");
         p2 = new Player("Johan");
@@ -18,6 +21,10 @@ public class testMainPlayer {
 
         System.out.println("TEST");
 
+
+        p1.addMoney(200);
+        p1.addCard(theDeck.dealCard());
+        System.out.println(p1.toString());
 
     }
 
