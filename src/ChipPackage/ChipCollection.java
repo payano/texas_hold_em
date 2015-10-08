1f8918804ffb4bbc1f67869800c4785293896c9d
johan <johan@exclude.se>
2015-10-08 16:50:17 +0200

    Comments in chip

--- src/ChipPackage/ChipCollection.java
+++ src/ChipPackage/ChipCollection.java
@@ -4,14 +4,20 @@ import java.util.ArrayList;
 
 /**
  * Created by Arvid Bodin and Johan Svensson on 2015-09-15.
- *
+ * ChipCollection class holds an arrayList of chips and methods for accessing them.
  */
 public class ChipCollection {
     ArrayList<Chip> theChips;
-
+    /**
+     * Constructor
+     */
     public ChipCollection(){
         theChips = new ArrayList<>();
     }
+    /**
+     * getChipsValue
+     * @return the amount the chips are worth.
+     */
     public int getChipsValue(){
         int result = 0;
         for(Chip s : theChips){
@@ -19,8 +25,18 @@ public class ChipCollection {
         }
         return result;
     }
+    /**
+     * addChips - add new chips to theChips
+     * @param chip
+     */
     //Inte klar... måste testas
+    //kan bygga en addChips som tar en int som input och gör om det till chips i olika valörer.
     public void addChips(Chip chip){theChips.add(chip);}
+    /**
+     * removeChips - removes one chip from the collection.
+     * @param id
+     * @return
+     */
     //INTE klar.. måste testas.
     public Chip removeChip(int id){
         if(id < 0 || id > theChips.size()){
@@ -28,6 +44,10 @@ public class ChipCollection {
         }
         return theChips.remove(id);
     }
+    /**
+     * toString - prints out chips
+     * @return
+     */
     public String toString(){
         String result = new String();
         for(Chip s : theChips){
