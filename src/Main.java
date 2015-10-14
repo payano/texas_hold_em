16d956c9da196127f4e7c783b6a1e2c63277cfb7
Urfen <arvidbod@kth.se>
2015-10-15 00:03:40 +0200

    Ny main

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,31 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import java.applet.Applet;
+
 /**
  * Created by arvidbodin on 14/09/15.
  *
  *
  */
 
-public class Main {
+public class Main extends Application{
+
+
 
     public static void main(String[] args) {
-        Game game = new Game();
-        game.start();
+        launch(args);
+        //Game game = new Game();
+        //game.start();
         //Skapar och startar spelet.
         //Game game = new Game();
         //game.playGame();
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        GameModel game = new GameModel();
+        game.start();
+
     }
 }
